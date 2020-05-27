7943f956270810db5af927019ab3a94cea7d3410
Zyla <erencana12@gmail.com>
2020-05-28 01:05:22 +0300

    Added ship command, but will be updated will images.

---
+++ src/commands/Social/ship.js
@@ -0,0 +1,44 @@
+const { Command } = require('discord-akairo');
+const Discord = require('discord.js');
+
+class ShippingCommand extends Command {
+	constructor() {
+		super('ship', {
+			aliases: ['ship', 'match'],
+			channel: 'guild',
+			cooldown: 20000,
+			description: {
+				description: 'Ships two people!',
+				usage: '`k!ship @user` or `k!match @firstPerson @secondPerson`',
+			},
+			args: [
+				{
+					id: 'firstMember',
+					type: 'memberMention',
+				},
+				{
+					id: 'secondMember',
+					type: 'memberMention',
+				},
+			],
+			ownerOnly: true,
+		});
+	}
+	async exec(message, args) {
+		if (!args.firstMember || !args.secondMember) return;
+
+		const love = Math.random() * 100;
+		const loveIndex = Math.floor(love / 10);
+		const loveLevel = '💖'.repeat(loveIndex) + '💔'.repeat(10 - loveIndex);
+		const shippingEmbed = new Discord.MessageEmbed()
+			.setColor('dd2e44')
+			.setTitle('Shipping...')
+			.setDescription(
+				`${args.firstMember} loves ${args.secondMember} this much!`,
+			)
+			.addField(`Their love ratio is: ${loveIndex}/10`, `${loveLevel}`);
+		message.channel.send(shippingEmbed);
+	}
+}
+
+module.exports = ShippingCommand;
