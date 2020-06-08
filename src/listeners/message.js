35db5e4858dd6c14f99459a0f7822cd545c03c55
Zyla <erencana12@gmail.com>
2020-06-09 02:09:45 +0300

    I'm lost.

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -1,16 +1,17 @@
-const { Listener } = require('discord-akairo');
-const Discord = require('discord.js');
+const {
+  Listener
+} = require("discord-akairo");
+const Discord = require("discord.js");
 
 class MessageListener extends Listener {
-	constructor() {
-		super('message', {
-			emitter: 'client',
-			event: 'message',
-			category: 'Client',
-			type: 'on',
-		});
-	}
-	exec(message) {}
+  constructor() {
+    super("message", {
+      emitter: "client",
+      event: "message",
+      category: "Client"
+    });
+  }
+  exec(message) {}
 }
 
 // module.exports = MessageListener;
